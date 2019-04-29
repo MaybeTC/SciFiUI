d178b93f08dc5d9eab60286e23acd1d467c43844
MaybeTCC <d16122910@mydit.ie>
2019-04-29 19:37:42 +0800

    using new method to draw the clock

--- java/src/ie/tudublin/Clock.java
+++ java/src/ie/tudublin/Clock.java
@@ -21,6 +21,44 @@ public class Clock
 
     void drawclock()
     {   
+        // dial 
+        ui.ellipseMode(ui.CENTER);
+        ui.strokeWeight(10);
+        ui.fill(255);
+        ui.ellipse(x, y, cd, cd);
+
+        // scale lines
+        for(int i = 1; i <= 60; i++) 
+        {
+            ui.pushMatrix();  // Coordinate system location
+            ui.translate(x, y);  // Moving coordinate system(0,0)to(width/2, height/2)
+            ui.rotate(ui.radians(i * 6));
+            if(i % 5 == 0) 
+            { 
+                // The tick mark of the whole point
+                ui.strokeWeight(10);
+                ui.stroke(0);
+                ui.line(cd * .4f, 0, cd * .433f, 0);
+            } 
+            else 
+            { 
+                // Non-integral tick mark
+                ui.strokeWeight(5);
+                ui.stroke(0);
+                ui.line(cd * .416f, 0, cd * .433f, 0);
+            }
+                ui.popMatrix();  // Restore coordinate system position
+        }
+        //number
+        ui.textAlign(PApplet.CENTER, PApplet.CENTER);
+        ui.textSize(30);
+        ui.fill(0);
+        ui.text(12, x, y - cd / 2 + 90);
+        ui.text(3, x + cd / 2 - 90, y);
+        ui.text(6, x, y + cd / 2 - 90);
+        ui.text(9, x - cd / 2 + 90, y);
+
+        /*
         //Housing washer of clock
         ui.fill(255);
         ui.stroke(255);
@@ -58,6 +96,7 @@ public class Clock
         //minute hand
         //ui.quad(38, 31, 86, 20, 69, 63, 30, 76);
         //ui.quad(38, 31, 86, 20, 69, 63, 30, 76);
+        */
     }
 
     public void render()
